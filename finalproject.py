b5dcde688235b0ea7488894736e80f7041403853
matx17 <matx17@student.bth.se>
2018-10-27 19:25:19 +0000

    changes in mac

--- finalproject.py
+++ finalproject.py
@@ -5,17 +5,19 @@ vl=10
 session=Session(hostname=ip,community='public',version=2)
 macs=session.walk(".1.3.6.1.2.1.17.4.3.1.1")
 ports=session.walk(".1.3.6.1.2.1.17.4.3.1.2")
-connection = sqlite3.connect(':memory')
+connection = sqlite3.connect(':memory:')
 cursordata=connection.cursor()
-createtable="CREATE TABLE IF NOT EXISTS project(PORTS, VLAN, MACS,IP)"
+createtable="CREATE TABLE IF NOT EXISTS project(PORTS, VLAN, MACS, IP)"
 cursordata.execute(createtable)
 for x,y in zip(ports,macs):
     oid=y.oid
     oid_index=y.oid_index
     snmp_type=y.snmp_type
-    mac=y.value
+    mac=':'.join('{:02x}'.format(ord(p)) for p in y.value)
+    print(">>>>mac=",repr(mac))
     port=x.value
-    print ip, mac, port
-    cursordata.execute("INSERT INTO project(PORTS,VLAN,MACS,IP) VALUES(?,?,?,?)",(port,vl,mac,ip)
+    print ip,mac,port
+    cursordata.execute("INSERT INTO project(PORTS,VLAN, MACS, IP) VALUES(?,?,?,?)",(port,vl,mac,ip))
     connection.commit()
 connection.close()
+
